5435a4e9dde9f271e956ccf61f2e2b4386a172a2
MostafaZayed72 <zayed8646@gmail.com>
2024-01-15 13:48:43 +0200

    nu

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,7 +1,17 @@
 <template>
   <v-container
     ><v-row
-      ><v-col cols="6"></v-col
+      ><v-col cols="6" class="my-4 flex gap-5 align-center ml-10"
+        ><h1
+          class="text-3xl text-sky-700 mb-6 text-center"
+          style="font-size: 50px; font-weight: bold"
+        >
+          I'm
+        </h1>
+        <div
+          class="your-element-selector text-3xl text-green-500 text-center"
+          style="font-size: 30px; font-weight: bold"
+        ></div> </v-col
       ><v-col cols="12" lg="6"
         ><img
           class="rounded-full move-up-down"
@@ -9,6 +19,28 @@
           alt="" /></v-col></v-row
   ></v-container>
 </template>
+
+<script setup>
+import Typed from "typed.js";
+import { onMounted, onBeforeUnmount } from "vue";
+
+const options = {
+  strings: ["Mostafa Zayed", "a web developer"],
+  typeSpeed: 100, // سرعة الكتابة بالمللي ثانية
+  backSpeed: 20, // سرعة الرجوع بالمللي ثانية
+  loop: true, // تكرار النص
+};
+
+let typed;
+
+onMounted(() => {
+  typed = new Typed(".your-element-selector", options);
+});
+
+onBeforeUnmount(() => {
+  typed.destroy();
+});
+</script>
 
 <style>
 @keyframes moveUpDown {
